0e9e56f4126d945dd8fc43e4336ce33f05464cff
leticialopesmr <leticialopesmr@gmail.com>
2021-11-15 15:58:20 -0300

    Versão do projeto com os blocos ja preenchidos, css e js feitos

---
+++ JS/cadastroJS.js
@@ -0,0 +1,18 @@
+// Pagina JS da pagina cadastro.html .............
+//Ana Letícia Lopes Moreira    RA:2018013951
+
+function checkForm() {
+    var password = document.getElementById("password").value;
+    var passwordCheck = document.getElementById("passwordCheck").value;
+
+    if (password != passwordCheck) {
+        alert("Senhas diferentes, por favor confira sua senha!");
+        return false;
+    } else if (password == '' || passwordCheck == '') {
+        alert("Por favor preencha os campos de senha e confirme a senha!");
+        return false;
+    } else {
+        document.getElementById("registro").submit();
+        return true;
+    }
+};
